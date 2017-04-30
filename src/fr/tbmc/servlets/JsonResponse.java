96015b0b212c8da61c65eaafc4755c9e25066c7e
Thibault Ballier <me@tbmc.fr>
2017-04-30 12:55:17 +0200

    Comment JsonResponse

--- src/fr/tbmc/servlets/JsonResponse.java
+++ src/fr/tbmc/servlets/JsonResponse.java
@@ -12,14 +12,32 @@ import java.io.PrintWriter;
  */
 public class JsonResponse
 {
-    public static void r(HttpServletResponse resp, Object o) throws IOException
+    /**
+     * Encode object in JSON and write it in the output stream
+     * @param resp Response object pass in parameter at the method doGet or doPost or any other method doXxx
+     * @param object Object to convert in JSON
+     * @throws IOException
+     */
+    public static void r(HttpServletResponse resp, Object object) throws IOException
     {
+        // Create a Gson object
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
 
+        // Define the content type of the response
+        // We return json so we tell the client that the response is JSON
         resp.setContentType("application/json");
+
+        // Write json in the output stream
         PrintWriter out = resp.getWriter();
-        out.print(gson.toJson(o));
+
+        // Convert object in JSON
+        String jsonToSend = gson.toJson(object);
+
+        // Write JSON object in the output stream
+        out.print(jsonToSend);
+
+        // Force the writer to write in the output stream
         out.flush();
     }
 }
