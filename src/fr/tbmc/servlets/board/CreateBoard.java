5f4508674e9623ef65bda9be2316352610917920
Thibault Ballier <me@tbmc.fr>
2017-04-30 13:07:22 +0200

    Comment CreatedBoards

--- src/fr/tbmc/servlets/board/CreateBoard.java
+++ src/fr/tbmc/servlets/board/CreateBoard.java
@@ -17,24 +17,41 @@ import java.util.Map;
  */
 public class CreateBoard extends HttpServlet
 {
-
+    /**
+     * Create a new board if the name is not already taken
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
+        // Get the name pass in parameter
         String name = req.getParameter("name");
+
+        // Get the instance of CreatedBoards
         CreatedBoards boards = CreatedBoards.getInstance();
+
+        // Try to create a new board
         boolean creationSuccessful = boards.createBoard(name);
 
+        // Set the type of the response
         resp.setContentType("text");
         PrintWriter out = resp.getWriter();
 
+
         if(creationSuccessful) {
+            // If the creation successful
+            // Send a "OK 200" status code and send no text
             resp.setStatus(200);
             out.print("");
         }else {
+            // If the creation is not successful
             resp.setStatus(403);
             out.print("Error: board already exists");
         }
+        // Force the writer to write in the output stream
         out.flush();
     }
 
@@ -48,12 +65,19 @@ public class CreateBoard extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
+        // Get the parameters
         String name = req.getParameter("name");
         String pseudo = req.getParameter("pseudo");
+
+        // Get the instance of CreatedBoards
         CreatedBoards boards = CreatedBoards.getInstance();
+
+        // Create a HashMap to return a JSON object
         Map<String, Boolean> out = new HashMap<>();
         out.put("name", boards.boardExists(name));
         out.put("pseudo", boards.boardHasPseudo(name, pseudo));
+
+        // Send the response to the client
         JsonResponse.r(resp, out);
     }
 }
